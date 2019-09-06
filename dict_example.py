a40ec741353bbb9763115fd4a4b93e6ec460f089
acqant <acqant@users.noreply.github.com>
2019-09-06 15:53:23 -0400

    let's do this

---
+++ dict_example.py
@@ -0,0 +1,20 @@
+import datetime
+my_dict = {}
+
+my_dict[1] = 'I exist'
+my_dict[2] = 'second entry'
+
+#checking if a key exists
+def my_function(id, my_dict):
+    if id in my_dict:
+        print(f'\t{my_dict[1]}')
+    else:
+        print(f'\tNo key {id} found in my_dict')
+
+print("When key exists:")
+my_function(1,my_dict)
+print("When key does NOT exist:")
+my_function(10,my_dict)
+
+print(f'Print all items with my_dict.items:\n {my_dict.items()}')
+print(f'Print all keys with my_dict.items:\n {my_dict.keys()}')
